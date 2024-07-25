4c2ff295939349989c59ad075d645ec83afbf790
Vlad <vladanalyst@gmail.com>
2024-07-25 09:52:47 +0300

    задание 16.2 выполнено

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script>
+import { ref } from 'vue'
+import io from 'socket.io-client';
+const socket = io('http://localhost:5173');
+
+export default {
+  name: 'Homeview',
+  components:'HelloWorld',
+  data() {
+    return {
+      message: null
+    }
+  },
+  methods: {
+    send() {
+      socket.emit('message', this.message);
+    }
+  }
+};
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+  <div>
+    <input type="text" v-model="message">
+    <button @click="send">Отправить</button>
+  </div>
+</template>
